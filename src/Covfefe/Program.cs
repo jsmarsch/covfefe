7133141c5c96abee4c8e4bfadcb6932bd6af80a6
Jeremy Marsch <jeremy>
2019-03-09 17:42:57 -0600

    Bug fix:  Prevent multiple instances of the app from running.

--- src/Covfefe/Program.cs
+++ src/Covfefe/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,19 +10,34 @@ namespace Covfefe
 {
     static class Program
     {
+        private static Mutex SingleInstanceMutex;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            // ok, so you don't really need a form to do this -- you could use an ApplicationContext, but I just didn't feel like hand-coding all of the menu and resource stuff, so I used the form and the visual designers
+            // only allow 1 instance of this app to run at a time
+            SingleInstanceMutex = new Mutex(false, Assembly.GetEntryAssembly().FullName);
+            if (!SingleInstanceMutex.WaitOne(0))
+                return;
 
-            // todo:  add a container
-            var facade = new SleepManagementFacade(new Windows7PowerManagementEngine());
-            Application.Run(new TrayForm(facade));
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                // ok, so you don't really need a form to do this -- you could use an ApplicationContext, but I just didn't feel like hand-coding all of the menu and resource stuff, so I used the form and the visual designers
+
+                // todo:  add a container
+                var facade = new SleepManagementFacade(new Windows7PowerManagementEngine());
+                Application.Run(new TrayForm(facade));
+            }
+            finally
+            {
+                SingleInstanceMutex.ReleaseMutex();
+            }
         }
+
     }
 }

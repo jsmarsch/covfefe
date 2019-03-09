312ee6851ac1a670c32ed7178d02cd7e0d8f0155
Jeremy Marsch <jeremy>
2019-03-09 17:56:21 -0600

    Separate interface and class

--- src/Covfefe/ISleepManagementFacade.cs
+++ src/Covfefe/ISleepManagementFacade.cs
@@ -14,40 +14,4 @@ namespace Covfefe
         void DelaySleepOnBatteryStateChange(TimeSpan delay);
         void DelaySleepOnLidClose(TimeSpan delay);
     }
-
-    // orchestrates sleep operations
-    public class SleepManagementFacade: ISleepManagementFacade
-    {
-        private readonly IPowerManagementEngine _powerManagementEngine;
-
-        public SleepManagementFacade(IPowerManagementEngine powerManagementEngine)
-        {
-            _powerManagementEngine = powerManagementEngine;
-        }
-
-        public void KeepMonitorsAwake()
-        {
-            _powerManagementEngine.KeepMonitorsOn();
-        }
-
-        public void DisableSleep()
-        {
-            _powerManagementEngine.PreventSleep();
-        }
-
-        public void ClearSleepOverrides()
-        {
-            _powerManagementEngine.ClearPowerManagementOverrides();
-        }
-
-        public void DelaySleepOnBatteryStateChange(TimeSpan delay)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DelaySleepOnLidClose(TimeSpan delay)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ src/Covfefe/SleepManagementFacade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Covfefe
+{
+    public class SleepManagementFacade: ISleepManagementFacade
+    {
+        private readonly IPowerManagementEngine _powerManagementEngine;
+
+        public SleepManagementFacade(IPowerManagementEngine powerManagementEngine)
+        {
+            _powerManagementEngine = powerManagementEngine;
+        }
+
+        public void KeepMonitorsAwake()
+        {
+            _powerManagementEngine.KeepMonitorsOn();
+        }
+
+        public void DisableSleep()
+        {
+            _powerManagementEngine.PreventSleep();
+        }
+
+        public void ClearSleepOverrides()
+        {
+            _powerManagementEngine.ClearPowerManagementOverrides();
+        }
+
+        public void DelaySleepOnBatteryStateChange(TimeSpan delay)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DelaySleepOnLidClose(TimeSpan delay)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
